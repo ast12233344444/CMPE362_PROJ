5ac9d2b2de72b1a06f45dea020670cfb034f062c
Yusuf AKIN <akinyusuf17@gmail.com>
2025-05-28 17:30:27 +0300

    feat: add progress bars

--- +basic_compression/compress.m
+++ +basic_compression/compress.m
@@ -1,24 +1,27 @@
 function [compressed_data, layer_sizes, frame_to_block, block_size] = compress(images, quantization_matrix, GOP_size)
     mblocks = [];
-    %convert images to blocks
-    for k = 1:length(images)
+    num_images = length(images);
+
+    h1 = waitbar(0, 'Converting images to blocks...');
+    for k = 1:num_images
+        waitbar(k / num_images, h1);
         if mod(k-1, GOP_size) == 0 
             image = int32(images{k});
             blocks = utils.frame_to_mb(image);
             mblocks{end+1} = blocks;
         else
             image_current = int32(images{k});
-            image_prev=  int32(images{k-1});
+            image_prev = int32(images{k-1});
             diff = image_current - image_prev;
             blocks = utils.frame_to_mb(diff);
             mblocks{end+1} = blocks;
         end
-    
     end
-        
-        
-    %convert blocks with DCT
-    for k = 1:length(images)
+    close(h1);
+
+    h2 = waitbar(0, 'Applying DCT to blocks...');
+    for k = 1:num_images
+        waitbar(k / num_images, h2);
         mblock = mblocks{k}; 
         for i = 1:size(mblock, 1)
             for j = 1:size(mblock, 2)
@@ -31,35 +34,39 @@ function [compressed_data, layer_sizes, frame_to_block, block_size] = compress(i
         end
         mblocks{k} = mblock;
     end
-        
-    
-        
-    for k = 1:length(images)
-       mblock = mblocks{k};
-       for i = 1:size(mblock, 1)
-           for j = 1:size(mblock, 2)
-               block = mblock{i, j};
-               R = block(:, :, 1);
-               G = block(:, :, 2);
-               B = block(:, :, 3);
-               R = R ./ quantization_matrix;
-               G = G ./ quantization_matrix;
-               B = B ./ quantization_matrix;
-               mblock{i, j} = cat(3, R, G, B);
+    close(h2);
+
+    h3 = waitbar(0, 'Quantizing blocks...');
+    for k = 1:num_images
+        waitbar(k / num_images, h3);
+        mblock = mblocks{k};
+        for i = 1:size(mblock, 1)
+            for j = 1:size(mblock, 2)
+                block = mblock{i, j};
+                R = block(:, :, 1);
+                G = block(:, :, 2);
+                B = block(:, :, 3);
+                R = R ./ quantization_matrix;
+                G = G ./ quantization_matrix;
+                B = B ./ quantization_matrix;
+                mblock{i, j} = cat(3, R, G, B);
             end
         end
         mblocks{k} = mblock;
     end
-    
+    close(h3);
+
     Rlayer_cell = {};
     Glayer_cell = {};
     Blayer_cell = {};
-    disp("size for mblock:"+ size(mblock));
-    for k = 1:length(images)
+
+    h4 = waitbar(0, 'Performing zigzag and run-length encoding...');
+    for k = 1:num_images
+        waitbar(k / num_images, h4);
         mblock = mblocks{k};
         for i = 1:size(mblock, 1)
             for j = 1:size(mblock, 2)
-                block = int8(mblock{i,j});
+                block = int8(mblock{i, j});
                 R = block(:, :, 1);
                 G = block(:, :, 2);
                 B = block(:, :, 3);
@@ -70,13 +77,14 @@ function [compressed_data, layer_sizes, frame_to_block, block_size] = compress(i
                 Grle = utils.run_length_encode(Gzb);
                 Brle = utils.run_length_encode(Bzb);
 
-                
                 Rlayer_cell{end+1} = Rrle;
                 Glayer_cell{end+1} = Grle;
                 Blayer_cell{end+1} = Brle;
             end
         end
     end
+    close(h4);
+
     Rlayer = vertcat(Rlayer_cell{:});
     Glayer = vertcat(Glayer_cell{:});
     Blayer = vertcat(Blayer_cell{:});
@@ -85,9 +93,9 @@ function [compressed_data, layer_sizes, frame_to_block, block_size] = compress(i
     Glayer_size = size(Glayer, 1);
     Blayer_size = size(Blayer, 1);
 
-    compressed_data = [ Rlayer; Glayer; Blayer];
+    compressed_data = [Rlayer; Glayer; Blayer];
     layer_sizes = [Rlayer_size, Glayer_size, Blayer_size];
     [brows, bcols] = size(mblocks{1});
-    frame_to_block = [length(images), brows, bcols];
+    frame_to_block = [num_images, brows, bcols];
     block_size = [8, 8];
 end
--- +basic_compression/decompress.m
+++ +basic_compression/decompress.m
@@ -6,12 +6,22 @@ function images = decompress(compressed_data, layer_sizes, frames_to_blocks, blo
     Glayer = compressed_data(Rsize+1:Rsize+Gsize, :);
     Blayer = compressed_data(Rsize+Gsize+1:Rsize+Gsize+Bsize, :);
 
+    h1 = waitbar(0, 'Decoding run-length encoded data...');
     Rlayer = utils.run_length_decode(Rlayer);
+    waitbar(1/3, h1);
     Glayer = utils.run_length_decode(Glayer);
+    waitbar(2/3, h1);
     Blayer = utils.run_length_decode(Blayer);
+    waitbar(1, h1);
+    close(h1);
 
     mblocks = [];
-    for k = 1:frames_to_blocks(1)
+    num_blocks = frames_to_blocks(1);
+
+    % Progress bar for inverse zigzag and block reconstruction
+    h2 = waitbar(0, 'Reconstructing blocks from compressed data...');
+    for k = 1:num_blocks
+        waitbar(k / num_blocks, h2);
         mblock = [];
         for i = 1:frames_to_blocks(2)
             for j = 1:frames_to_blocks(3)
@@ -26,34 +36,40 @@ function images = decompress(compressed_data, layer_sizes, frames_to_blocks, blo
                 R = utils.inverse_zigzag(Rzb);
                 G = utils.inverse_zigzag(Gzb);
                 B = utils.inverse_zigzag(Bzb);
-    
-                mblock{i, j} =  double(cat(3, R, G, B));
+
+                mblock{i, j} = double(cat(3, R, G, B));
             end
         end
         mblocks{end+1} = mblock;
     end
-    
-    
+    close(h2);
+
+    % Progress bar for dequantization
+    h3 = waitbar(0, 'Applying dequantization...');
     for k = 1:length(mblocks)
-       mblock = mblocks{k};
-       for i = 1:size(mblock, 1)
-           for j = 1:size(mblock, 2)
-               block = mblock{i, j};
-               R = block(:, :, 1);
-               G = block(:, :, 2);
-               B = block(:, :, 3);
-               R = R .* quantization_matrix;
-               G = G .* quantization_matrix;
-               B = B .* quantization_matrix;
-               mblock{i, j} = cat(3, R, G, B);
+        waitbar(k / length(mblocks), h3);
+        mblock = mblocks{k};
+        for i = 1:size(mblock, 1)
+            for j = 1:size(mblock, 2)
+                block = mblock{i, j};
+                R = block(:, :, 1);
+                G = block(:, :, 2);
+                B = block(:, :, 3);
+                R = R .* quantization_matrix;
+                G = G .* quantization_matrix;
+                B = B .* quantization_matrix;
+                mblock{i, j} = cat(3, R, G, B);
             end
         end
         mblocks{k} = mblock;
     end
-    
-    
+    close(h3);
+
+    % Progress bar for inverse DCT
+    h4 = waitbar(0, 'Applying inverse DCT...');
     for k = 1:length(mblocks)
-        mblock = mblocks{k}; 
+        waitbar(k / length(mblocks), h4);
+        mblock = mblocks{k};
         for i = 1:size(mblock, 1)
             for j = 1:size(mblock, 2)
                 block = mblock{i, j};
@@ -65,24 +81,34 @@ function images = decompress(compressed_data, layer_sizes, frames_to_blocks, blo
         end
         mblocks{k} = mblock;
     end
-    
-    
+    close(h4);
+
     images = [];
-    for k = 1:length(mblocks)   
-        if mod(k-1, GOP_size) == 0 
+    num_images = length(mblocks);
+
+    % Progress bar for reconstructing images
+    h5 = waitbar(0, 'Reconstructing images...');
+    for k = 1:num_images
+        waitbar(k / num_images, h5);
+        if mod(k-1, GOP_size) == 0
             mblock = mblocks{k};
             image = utils.mb_to_frame(mblock);
             images{end+1} = image;
         else
             mblock = mblocks{k};
-            image_prev= images{k-1};
-            diff =  utils.mb_to_frame(mblock);
+            image_prev = images{k-1};
+            diff = utils.mb_to_frame(mblock);
             image_current = image_prev + diff;
             images{end+1} = image_current;
         end
     end
-   
-    for k = 1:length(mblocks)
+    close(h5);
+
+    % Progress bar for converting images to uint8
+    h6 = waitbar(0, 'Converting images to uint8...');
+    for k = 1:num_images
+        waitbar(k / num_images, h6);
         images{k} = uint8(images{k});
     end
+    close(h6);
 end
